328c70909e2aedf456419152231a2d8e8c27d59f
wildaimama <wildaimama@gmail.com>
2024-02-25 15:59:33 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Training Day 1',
+    'summary': 'Day 1 Batch 39',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/contact_view.xml',
+    ],
+    'license': 'LGPL-3',
+}
